f4149b905d0076a77455c1055dc5308db452f44c
Jaydan Urwin <jaydan@jaydanurwin.com>
2022-12-25 09:22:06 -0700

    added footer

--- src/christmas-quiz.ts
+++ src/christmas-quiz.ts
@@ -31,7 +31,6 @@ export class ChristmasQuiz extends LitElement {
         padding: 1rem;
         /* From https://css.glass */
         background: rgba(255, 255, 255, 0.65);
-        border-radius: 16px;
         box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
         backdrop-filter: blur(7.6px);
         -webkit-backdrop-filter: blur(7.6px);
